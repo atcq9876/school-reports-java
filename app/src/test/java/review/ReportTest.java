150fd9e9f197258927ae1299c63678848efa11c9
Andy <ajscottlewis@gmail.com>
2023-03-17 12:41:41 +0000

    Update format() method name in tests

--- app/src/test/java/review/ReportTest.java
+++ app/src/test/java/review/ReportTest.java
@@ -7,30 +7,30 @@ public class ReportTest {
     @Test public void testSingleGreenInput() {
       String grade = "Green";
       Report report = new Report(grade);
-      assertEquals(report.format(), "Green: 1");
+      assertEquals(report.getFormattedReport(), "Green: 1");
     }
 
     @Test public void testSingleAmberInput() {
       String grade = "Amber";
       Report report = new Report(grade);
-      assertEquals(report.format(), "Amber: 1");
+      assertEquals(report.getFormattedReport(), "Amber: 1");
     }
 
     @Test public void testSingleRedInput() {
       String grade = "Red";
       Report report = new Report(grade);
-      assertEquals(report.format(), "Red: 1");
+      assertEquals(report.getFormattedReport(), "Red: 1");
     }
 
     @Test public void testUncountedValue() {
       String grade = "blah";
       Report report = new Report(grade);
-      assertEquals(report.format(), "Uncounted: 1");
+      assertEquals(report.getFormattedReport(), "Uncounted: 1");
     }
 
     @Test public void testTwoGreenValues() {
       String grade = "Green, Green";
       Report report = new Report(grade);
-      assertEquals(report.format(), "Green: 2");
+      assertEquals(report.getFormattedReport(), "Green: 2");
     }
 }
